ed1ce94d2a750801be0b13ce47a7ea6a24058a3d
jaysonocharo <124657684+jaysonocharo@users.noreply.github.com>
2026-03-13 17:08:55 +0300

    Add Candidate struct and related functions
    Final update

--- voter.h
+++ voter.h
@@ -5,14 +5,14 @@
 #define MAX_ID 15
 #define MAX_POSITION 50 // Added for candidate positions
 
-// Existing Voter Struct
+//  Voter Struct
 typedef struct {
     char student_id[MAX_ID];
     char name[MAX_NAME];
     int has_voted; // 0 for No, 1 for Yes
 } Voter;
 
-// New Candidate Struct
+//  Candidate Struct
 typedef struct {
     char student_id[MAX_ID];
     char name[MAX_NAME];
@@ -24,11 +24,10 @@ void register_voter();
 void display_voters();
 int is_id_registered(const char* id);
 
-// New Candidate Prototypes
+// Candidate Prototypes
 void register_candidate();
 int is_candidate_registered(const char* id);
 
-// Add this new prototype
 void display_candidates();
 
 #endif
